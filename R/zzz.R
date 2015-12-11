ebb6f166d062ea8839053215940ce55dce08ad88
bhoff <bruce.hoff@sagebase.org>
2015-12-11 15:28:10 -0800

    correctly builds/installs on Mac

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
 #########################################################
 
 .onLoad <- function( libname, pkgname ){
-
+	Sys.setenv(PYTHONHOME=system.file(package="rPython"))
+	Sys.setenv(PYTHONPATH=system.file("lib", package="rPython"))
 	# 'local=FALSE' is crucial, otherwise various shared objects which
 	# are part of Python modules will not have access to the symbols in libpython
 	library.dynam( "rPython", pkgname, libname, local=FALSE)
