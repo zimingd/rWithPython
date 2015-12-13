6f6bd6087d1951456d7a9816c16eac8b562dadb9
bhoff <bruce.hoff@sagebase.org>
2015-12-13 09:47:55 -0800

    changed rPython to rWithPython

--- R/python.exec.R
+++ R/python.exec.R
@@ -5,12 +5,12 @@
 python.exec <- function( python.code, get.exception = TRUE ){
 
     if( ! get.exception ){
-        exit.status <- .C( "py_exec_code", python.code, exit.status = integer(1), PACKAGE = "rPython" )$exit.status
+        exit.status <- .C( "py_exec_code", python.code, exit.status = integer(1), PACKAGE = "rWithPython" )$exit.status
         return( exit.status )
     }
 
     # removel the previous exception in python, if any
-    .C( "py_exec_code", "try:del _r_error\nexcept Exception:pass", exit.status = integer(1), PACKAGE = "rPython" )
+    .C( "py_exec_code", "try:del _r_error\nexcept Exception:pass", exit.status = integer(1), PACKAGE = "rWithPython" )
 
     # Creating the call; the call is embeded into a try-catch expression in order to capture possible exceptions
     if( length( python.code ) == 1 )
@@ -19,9 +19,9 @@ python.exec <- function( python.code, get.exception = TRUE ){
     python.code <- as.character( sapply( python.code, function( x ) paste( "\t", x, sep = "" ) ) )
     python.code <- paste( "try:", paste( python.code, collapse = "\n" ), "except Exception as e:_r_error = e.__str__()\n", sep = "\n" )
 
-    exit.status <- .C( "py_exec_code", python.code, exit.status = integer(1), PACKAGE = "rPython" )$exit.status          # Exit status always good as any exception is captured
+    exit.status <- .C( "py_exec_code", python.code, exit.status = integer(1), PACKAGE = "rWithPython" )$exit.status          # Exit status always good as any exception is captured
 
-    ret <- .C( "py_get_var", "_r_error", no.error = integer(1), error.desc = character(1), PACKAGE = "rPython" )
+    ret <- .C( "py_get_var", "_r_error", no.error = integer(1), error.desc = character(1), PACKAGE = "rWithPython" )
     if( ! ret$no.error )
         stop( ret$error.desc )
 
--- R/zzz.R
+++ R/zzz.R
@@ -3,15 +3,15 @@
 #########################################################
 
 .onLoad <- function( libname, pkgname ){
-	Sys.setenv(PYTHONHOME=system.file(package="rPython"))
-	Sys.setenv(PYTHONPATH=system.file("lib", package="rPython"))
+	Sys.setenv(PYTHONHOME=system.file(package="rWithPython"))
+	Sys.setenv(PYTHONPATH=system.file("lib", package="rWithPython"))
 	# 'local=FALSE' is crucial, otherwise various shared objects which
 	# are part of Python modules will not have access to the symbols in libpython
-	library.dynam( "rPython", pkgname, libname, local=FALSE)
-  .C( "py_init", PACKAGE = "rPython" )
+	library.dynam( "rWithPython", pkgname, libname, local=FALSE)
+  .C( "py_init", PACKAGE = "rWithPython" )
 }
 
 .onUnload <- function( libpath ){
-    .C( "py_close", PACKAGE = "rPython" )
-    library.dynam.unload( "rPython", libpath )
+    .C( "py_close", PACKAGE = "rWithPython" )
+    library.dynam.unload( "rWithPython", libpath )
 }
