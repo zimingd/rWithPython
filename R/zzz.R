36d8887bbff1b756afde35cf337721dc856e85ae
bhoff <bruce.hoff@sagebase.org>
2015-12-28 09:25:06 -0800

    debug info

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 .onLoad <- function( libname, pkgname ) {
 	cat(sprintf("in '.onLoad':\n\tlibname: <%s>\n\tpkgname: <%s>\n", libname, pkgname))
 	cat("in '.onLoad':  Contents of 'libs' folder:\n")
-	cat(list.files(system.file("libs", package="rWithPython"), recursive=T, sep="\n\t"), "\n")
+	cat(list.files(system.file("libs", package="rWithPython"), recursive=T), sep="\n\t")
 	Sys.setenv(PYTHONHOME=system.file(package="rWithPython"))
 	Sys.setenv(PYTHONPATH=system.file("lib", package="rWithPython"))
 	# 'local=FALSE' is crucial, otherwise various shared objects which
