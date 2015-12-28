1fe305fd0e342c5cdf1915fe36ae40fc87c4c76e
bhoff <bruce.hoff@sagebase.org>
2015-12-28 08:46:50 -0800

    debug info

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,20 @@
 #########################################################
 # CGB, 20100716
 #########################################################
+printfiles<-function(dir, indent) {
+	cat(indent, dir, "\n")
+	if (file.info(dir)['isdir']) {
+		for (file in list.files(dir)) {
+			printfiles(file, paste(indent, "\t", sep=""))
+		}
+	}
+}
 
 .onLoad <- function( libname, pkgname ) {
 	cat(sprintf("in '.onLoad':\n\tlibname: <%s>\n\tpkgname: <%s>\n", libname, pkgname))
-	cat("in '.onLoad':  Contents of 'libs' folder:\n\t", list.files(system.file("libs", package="rWithPython")), "\n")
+	cat("in '.onLoad':  Contents of 'libs' folder:\n")
+	printfiles(system.file("libs", package="rWithPython"), "\t")
+	cat("\n")
 	Sys.setenv(PYTHONHOME=system.file(package="rWithPython"))
 	Sys.setenv(PYTHONPATH=system.file("lib", package="rWithPython"))
 	# 'local=FALSE' is crucial, otherwise various shared objects which
