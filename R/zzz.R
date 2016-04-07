c0215338a9ac4fed6d106018c30270d5cea97b9e
bhoff <bruce.hoff@sagebase.org>
2016-04-07 07:00:45 -0700

    try loading all python dlls in .onload()

--- R/zzz.R
+++ R/zzz.R
@@ -2,20 +2,24 @@
 # CGB, 20100716
 #########################################################
 
-# on Windows we need to add Python dll's to library search path
-addPythonLibrariesToWindowsPath<-function(libname, pkgname) {
-	if (Sys.info()['sysname']!="Windows") return
+pathToPythonLibraries<-function() {
 	# Note: 'pythonLibs' is defined in configure.win
 	pathToPythonLibraries<-file.path(libname, pkgname, "pythonLibs")
 	pathToPythonLibraries<-gsub("/", "\\", pathToPythonLibraries, fixed=T)
+	
+}
+
+# on Windows we need to add Python dll's to library search path
+addPythonLibrariesToWindowsPath<-function(libname, pkgname) {
+	if (Sys.info()['sysname']!="Windows") return
 #	currentPathString<-Sys.getenv("PATH")
-	pathSep<-";"
+#	pathSep<-";"
 #	currentPaths<-strsplit(currentPathString, pathSep, fixed=T)[[1]]
 #  if (!any(currentPaths==pathToPythonLibraries)) {
 #		newPathString<-paste(pathToPythonLibraries, currentPathString, sep=pathSep)
 #		Sys.setenv(PATH=newPathString)
 #	}
-	Sys.setenv(PATH=pathToPythonLibraries)
+	Sys.setenv(PATH=pathToPythonLibraries())
 #	cat("In addPythonLibrariesToWindowsPath, 'machine': ", Sys.info()['machine'], "\n")
 #	if (length(grep("64",  Sys.info()['machine'], fixed=T))==0) {
 #		# i386
@@ -35,6 +39,12 @@ addPythonLibrariesToWindowsPath<-function(libname, pkgname) {
 	# 'local=FALSE' is crucial, otherwise various shared objects which
 	# are part of Python modules will not have access to the symbols in libpython
 	library.dynam( "rWithPython", pkgname, libname, local=FALSE)
+	
+	# This is an experiment.  Does loading up all the Python dlls
+	dlls <- list.files(path=pathToPythonLibraries(), pattern="dll$", full.names=TRUE, recursive=TRUE, ignore.case=TRUE)
+	for (dll in dlls) {
+		dyn.load(dlls, local=FALSE, now=TRUE)
+	}
   .C( "py_init", PACKAGE = "rWithPython" )
 }
 
