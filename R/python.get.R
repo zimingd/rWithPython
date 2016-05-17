0177cecec42e32772598f2f91555443aa37c8a9f
bhoff <bruce.hoff@sagebase.org>
2016-05-17 15:02:36 -0700

    serialize MutableMappings

--- R/python.get.R
+++ R/python.get.R
@@ -4,7 +4,10 @@
 
 python.get <- function( var.name ){
 
-    python.command <- paste( "_r_return = json.dumps( [", var.name, "] )", sep = "" )
+		python.command <- paste( "from collections import MutableMapping\nif (isinstance(",var.name,
+				", MutableMapping)):\n\t_as_dict = dict(",var.name,
+				")\nelse:\n\t_as_dict = ",var.name,
+				"\n_r_return = json.dumps( [_as_dict] )", sep = "" )
     python.exec( python.command, get.exception = FALSE )
 
     ret <- .C( "py_get_var", "_r_return", not.found.var = integer(1), resultado = character(1), PACKAGE = "rWithPython" )
